5fd3ff6fb10647ba9d65996696e22eb14724585f
upendrasingh-63 <adshivam4040@gmail.com>
2024-09-29 22:39:10 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
+const __dirname = path.resolve();
 
 
 app.use(express.json());// to parse incoming request with json payload (form req.body)
@@ -23,6 +25,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 
