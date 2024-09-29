369e59a07d0feccd1323983d7816f2604a6aa9fc
upendrasingh-63 <adshivam4040@gmail.com>
2024-09-29 23:11:48 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://127.0.0.1:5000", {
+      const socket = io("https://pulsetalk-58ps.onrender.com/", {
         query: {
           userId: authUser._id,
         },
